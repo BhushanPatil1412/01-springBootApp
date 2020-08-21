49b7384878603dc75aaf92136b83ddc8e62f7291
BhushanPatil1412 <bhushanpatil1177@gmail.com>
2020-08-21 20:47:08 +0530

    feaur-102 commit

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -8,6 +8,30 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+
+
+
+
+
+
+
+
+
+
+
+
+public staticvoid add() {
+
+}
+
+
+
+
+
+
+
+
 	}
 
 }
